a1e65ad216941e6b801bba50026a0afa8bd4349e
Steven Adi Santoso <sstevenadi@gmail.com>
2023-11-06 14:27:27 +0700

    First commit

---
+++ src/test/java/com/silvering/sampleweb/SampleWebSampleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvering.sampleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleWebSampleWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
